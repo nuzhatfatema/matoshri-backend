fdce920c0f70ae8dfee7e5299cd086368ec2c134
nuzhat <nfatema9322@gmail.com>
2026-08-24 04:25:22 +0530

    Fix production CORS

--- server.js
+++ server.js
@@ -21,7 +21,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://matoshri-frontend.vercel.app",
+    ],
     credentials: true,
   })
 );
